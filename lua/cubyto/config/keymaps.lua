ab9e8c1a6126c9d34c8d38f89411d6b476a007d2
Cubyto <emiliocube165@gmail.com>
2023-10-15 00:18:37 -0500

    Add more plugins, deleted buferline

--- lua/cubyto/config/keymaps.lua
+++ lua/cubyto/config/keymaps.lua
@@ -23,8 +23,12 @@ keymap.set("n", "<leader>-", "<C-x>", { noremap = true }, { desc = "Decrement nu
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { noremap = true }, { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { noremap = true }, { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>so", "<C-w>s", { noremap = true }, { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { noremap = true }, { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sj", "<C-w>j", { noremap = true }, { desc = "Move cursor to down split"}) -- Move cursor to down split
+keymap.set("n", "<leader>sk", "<C-w>k", { noremap = true }, { desc = "Move cursor to up split"}) -- Move cursor to up split
+keymap.set("n", "<leader>sh", "<C-w>h", { noremap = true }, { desc = "Move cursor to right split"}) -- Move cursor to right split
+keymap.set("n", "<leader>sl", "<C-w>l", { noremap = true }, { desc = "Move cursor to left split"}) -- Move cursor to left split
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { noremap = true }, { desc = "Close current split" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { noremap = true }, { desc = "Open new tab" }) -- open new tab
